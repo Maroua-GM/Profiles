e04edf13437cd1b6469224bd56332c11d8156c15
Rabah GAG <rabah.bougag@hotmail.com>
2022-12-07 15:10:23 +0100

    modif css gag

---
+++ script.js
@@ -0,0 +1,21 @@
+let buton_Gag = document.getElementById("gag");
+buton_Gag.addEventListener("click", f_ouvrirLaPageGag);
+
+
+function f_ouvrirLaPageGag(){
+    buton_Gag.setAttribute("href", "./gag.html")
+    // open("./gag.html")
+    window.location.href="./gag.html"
+    console.log("clock")
+}
+document.getElementById("BMaroua").addEventListener('click',()=>{
+ window.open("CvMaroua.html")
+})
+
+document.getElementById("fatmaCV").addEventListener('click',()=>{
+    window.open("fatmaCV.html")
+   })
+
+document.getElementById("gaetan").addEventListener('click',()=>{
+    window.open("./Gaetan.html")
+})

fe4b9d4f6be4a46006207623a18a2d8788d85f80
Rabah GAG <rabah.bougag@hotmail.com>
2022-12-07 15:56:41 +0100

    meerge

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ document.getElementById("BMaroua").addEventListener('click',()=>{
  window.open("CvMaroua.html")
 })
 
-document.getElementById("lien_phil").addEventListener('click', () => {
-    window.open("cv_phil.html");    
-})
+// document.getElementById("lien_phil").addEventListener('click', () => {
+//     window.open("cv_phil.html");    
+// })
+
+let buton_Gag = document.getElementById("gag");
+buton_Gag.addEventListener("click", f_ouvrirLaPageGag);
+
+
+function f_ouvrirLaPageGag(){
+    buton_Gag.setAttribute("href", "./gag.html")
+    // open("./gag.html")
+    window.location.href="./gag.html"
+    console.log("clock")
+}

3884690523b11201c189dc95466337a1cfe191c5
Fatma Hocine <fatmahocine2014@gmail.com>
2022-12-07 14:15:40 +0100

    j'ai ajouté ma partie fatma

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("BMaroua").addEventListener('click',()=>{
  window.open("CvMaroua.html")
 })
+
+document.getElementById("fatmaCV").addEventListener('click',()=>{
+    window.open("fatmaCV.html")
+   })

1a523763859e56cfe0df00c4d5688e9b36b927bd
FrcGaetan <gaetan-91@hotmail.fr>
2022-12-07 16:38:01 +0100

    ajout rabah

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ buton_Gag.addEventListener("click", f_ouvrirLaPageGag);
 
 function f_ouvrirLaPageGag(){
     buton_Gag.setAttribute("href", "./gag.html")
-    // open("./gag.html")
+     open("./gag.html")
     window.location.href="./gag.html"
     console.log("clock")
 }

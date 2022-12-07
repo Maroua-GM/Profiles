688e238535a84d4649ea9b4b1d9132c292522e30
PhcchP <ph.cla.stagiaire@gmail.com>
2022-12-07 14:33:29 +0100

    modificaton du js window open cv_phil

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("BMaroua").addEventListener('click',()=>{
  window.open("CvMaroua.html")
 })
+
+document.getElementById("lien_phil").addEventListener('click', () => {
+    window.open("cv_phil.html");    
+})
